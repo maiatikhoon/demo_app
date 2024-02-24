080c6ddcd1a9a2447086abf6366383344a01e770
geekAtique <maiatikhoon@gmail.com>
2024-02-24 16:35:08 +0530

    added for loop

--- server.js
+++ server.js
@@ -10,3 +10,11 @@ console.log(" hello from prashant ") ;
 
 console.log(" hello from atique ") ; 
 console.log(" hello from neha ") ; 
+
+
+let array = [1,2,3,4] ;
+
+for (let index = 0; index < array.length; index++) {
+    let element = array[index];
+    
+}
